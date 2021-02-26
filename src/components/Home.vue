a55c9f8578ca7f0d42918f0c72cec5573ae5df3b
Lynch <766139427@qq.com>
2021-02-26 15:07:31 +0800

    实现登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    //退出功能
+    logout() {
+      //清空token
+      window.sessionStorage.clear();
+      this.$router.push('/login');
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://api.naccl.top/vue/shop/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
